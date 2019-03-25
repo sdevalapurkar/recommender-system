0584a32b836391cf3f3c343f52e096f29a6e221d
Rickus Senekal <rickussenekal@gmail.com>
2019-03-24 22:17:19 -0700

    gucci results

--- factorize_matrix.py
+++ factorize_matrix.py
@@ -53,78 +53,78 @@ def initialize_U_and_V():
     return U, V
 
 
-def matrix_factorization(M, U, V, T, d, alpha=0.01, beta=0.02):
+def matrix_factorization(M, U, V, T, d, non_zero_entries, alpha=0.01, beta=0.02):
     V = V.T
 
-    M_transposed = M.T
-
-    # print('M')
-    # print(M)
-    # print('U')
-    # print(U)
-    # print('V')
-    # print(V)
-
-    x = [0 for i in range(n)]
-    y = [0 for i in range(m)]
-
-    # for iter in range(T):
-    for k in range(d):
-        for i in range(n):
-            numerator_value = 0
-            denominator_value = 0
-
-            for j in range(m):
-                if (M[i][j] > 0):
-                    new_U_of_i_minus_k = np.delete(U[i,:], k)
-                    new_V_of_j_minus_k = np.delete(V[:,j], k)
-                    # print(k, i, j)
-                    # print(new_U_of_i_minus_k, new_V_of_j_minus_k)
-                    
-                    numerator_value = numerator_value + ((np.dot(new_U_of_i_minus_k, new_V_of_j_minus_k) - M[i][j]) * V[k][j])
-                    denominator_value = denominator_value + (V[k][j] ** 2)
-
-            x[i] = (-1 * numerator_value) / denominator_value
-
-        for i in range(n):
-            U[i, k] = x[i]
-
-    for k in range(d):
-        for j in range(m):
-            numerator_value = 0
-            denominator_value = 0
-
-            for i in range(n):
-                if (M_transposed[j][i] > 0):
-                    new_U_of_i_minus_k = np.delete(U[i,:], k)
-                    new_V_of_j_minus_k = np.delete(V[:,j], k)
-
-                    numerator_value = numerator_value + ((np.dot(new_U_of_i_minus_k, new_V_of_j_minus_k) - M[i][j]) * U[i][k])
-                    denominator_value = denominator_value + (U[i][k] ** 2)
-            
-            y[j] = (-1 * numerator_value) / denominator_value
-        
-        for j in range(m):
-            V[k][j] = y[j]
-
-    return U, V
-    # print('please work x')
-    # print(x)
-    # for iter in range(T):
+    # M_transposed = M.T
+    #
+    # # print('M')
+    # # print(M)
+    # # print('U')
+    # # print(U)
+    # # print('V')
+    # # print(V)
+    #
+    # x = [0 for i in range(n)]
+    # y = [0 for i in range(m)]
+    #
+    # # for iter in range(T):
+    # for k in range(d):
     #     for i in range(n):
+    #         numerator_value = 0
+    #         denominator_value = 0
+    #
     #         for j in range(m):
-    #             if M[i][j] > 0:
-    #                 eij = M[i][j] - np.dot(U[i,:],V[:,j])
-    #                 for k in range(d):
-    #                     U[i][k] = U[i][k] + alpha * (2 * eij * V[k][j] - beta * U[i][k])
-    #                     V[k][j] = V[k][j] + alpha * (2 * eij * U[i][k] - beta * V[k][j])
-    # return U, V.T
+    #             if (M[i][j] > 0):
+    #                 new_U_of_i_minus_k = np.delete(U[i,:], k)
+    #                 new_V_of_j_minus_k = np.delete(V[:,j], k)
+    #                 # print(k, i, j)
+    #                 # print(new_U_of_i_minus_k, new_V_of_j_minus_k)
+    #
+    #                 numerator_value = numerator_value + ((np.dot(new_U_of_i_minus_k, new_V_of_j_minus_k) - M[i][j]) * V[k][j])
+    #                 denominator_value = denominator_value + (V[k][j] ** 2)
+    #
+    #         x[i] = (-1 * numerator_value) / denominator_value
+    #
+    #     for i in range(n):
+    #         U[i, k] = x[i]
+    #
+    # for k in range(d):
+    #     for j in range(m):
+    #         numerator_value = 0
+    #         denominator_value = 0
+    #
+    #         for i in range(n):
+    #             if (M_transposed[j][i] > 0):
+    #                 new_U_of_i_minus_k = np.delete(U[i,:], k)
+    #                 new_V_of_j_minus_k = np.delete(V[:,j], k)
+    #
+    #                 numerator_value = numerator_value + ((np.dot(new_U_of_i_minus_k, new_V_of_j_minus_k) - M[i][j]) * U[i][k])
+    #                 denominator_value = denominator_value + (U[i][k] ** 2)
+    #
+    #         y[j] = (-1 * numerator_value) / denominator_value
+    #
+    #     for j in range(m):
+    #         V[k][j] = y[j]
+    #
+    # return U, V
+
+    for iter in range(T):
+        for i in range(n):
+            for j in range(m):
+                if M[i][j] > 0:
+                    eij = M[i][j] - np.dot(U[i,:],V[:,j])
+                    for k in range(d):
+                        U[i][k] = U[i][k] + alpha * (2 * eij * V[k][j] - beta * U[i][k])
+                        V[k][j] = V[k][j] + alpha * (2 * eij * U[i][k] - beta * V[k][j])
+        # RMSE(U, V.T, M, non_zero_entries)
+    return U, V.T
 
 
 def RMSE(nU, nV, M, non_zero_entries):
-    # nM = np.dot(nU, nV.T)
+    nM = np.dot(nU, nV.T)
 
-    nM = np.dot(nU, nV)
+    # nM = np.dot(nU, nV)
 
     sum = 0
     for i in range(n):
@@ -137,7 +137,6 @@ def RMSE(nU, nV, M, non_zero_entries):
 
 
 if __name__ == "__main__":
-    start_time = time.time()
 
     # For Toy Ratings Dataset
     # n = 3
@@ -149,10 +148,7 @@ if __name__ == "__main__":
     n = 943
     m = 1682
     d = 20
-    # n = 3
-    # m = 3
-    # d = 2
-    T = 20
+    T = 60 # T = 60 is gucci number with RMSE of 0.6163 and time of 4:13
 
     args = constructArguments()
     data = defaultdict(dict)
@@ -191,17 +187,10 @@ if __name__ == "__main__":
 
     U = np.random.rand(n,d)
     V = np.random.rand(m,d)
-
-    nU, nV = matrix_factorization(M, U, V, T, d)
-
-    # print('uT')
-    # print(nU)
-
-    # print('vT')
-    # print(nV)
+    start_time = time.time()
+    nU, nV = matrix_factorization(M, U, V, T, d, non_zero_entries)
 
     # nM = np.dot(nU, nV.T)
-
     # print("Original M (Ratings):")
     # print(M)
     # print("\n")
@@ -211,43 +200,6 @@ if __name__ == "__main__":
     RMSE(nU, nV, M, non_zero_entries)
     elapsed_time = time.time() - start_time
     print("Time elapsed:", time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+    np.savetxt('UT.tsv', nU, fmt='%.18e', delimiter='\t', newline='\n')
 
     # End of working code
-
-
-    # UV_decomposition(M,n,m,d)
-
-    # user_ratings_dict, toy_ratings_dict = generate_user_and_toy_dict(int_reader_list)
-    # x = [0 for i in range(n)]
-    #
-    # print(user_ratings_dict)
-    # print(toy_ratings_dict)
-    # print("\n")
-    # print(U)
-    # print("\n")
-    # print(V)
-    # print("\n")
-    #
-    # # for p in range(20):
-    # for k in range(0, d):
-    #     print("--Starting Iteration--")
-    #     for i in user_ratings_dict:
-    #         new_U_of_i = U[i]
-    #         new_V = V
-    #         new_U_of_i_minus_k = np.reshape(np.delete(new_U_of_i, k), (1, d - 1))
-    #         print(new_U_of_i_minus_k)
-    #
-    #         x[i] = -1 * sum(map(lambda j: (np.dot(new_U_of_i_minus_k, np.reshape(np.delete(new_V[:, j[0]], k), (d - 1, 1))) - j[1]) * V[k][j[0]], user_ratings_dict[i])) / sum(map(lambda j: V[k][j[0]] ** 2, user_ratings_dict[i]))
-    #     for j in user_ratings_dict[i]:
-    #         new_V_of_j = V[:, j[0]]
-    #
-    #         new_U_of_i_minus_k = np.delete(new_U_of_i, k)
-    #         new_V_of_j_minus_k = np.delete(new_V_of_j, k)
-    #
-    #         new_U_of_i_minus_k = np.reshape(new_U_of_i_minus_k, (1, d - 1))
-    #         new_V_of_j_minus_k = np.reshape(new_V_of_j_minus_k, (d - 1, 1))
-    #
-    #         x[i] += (-1 * (np.dot(new_U_of_i_minus_k, new_U_of_i_minus_k) - j[1]) * V[k][j[0]]) / (V[k][j[0]] ** 2)
-    #
-    # print("\n")
-    # print(x)
